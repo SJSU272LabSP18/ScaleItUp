74c4cd8f4ff087850fdaeb7d8cf80c1e8c7169f6
vidhya-vijayakumar <36356627+vidhya-vijayakumar@users.noreply.github.com>
2018-05-07 07:57:45 -0700

    Add files via upload

--- project272/marketing_team_frontend/client/components/notes/EditNoteForm.js
+++ project272/marketing_team_frontend/client/components/notes/EditNoteForm.js
@@ -3,15 +3,16 @@ import PropTypes from 'prop-types';
 import uuidv1 from 'uuid/v1';
 
 
-class AddNoteForm extends Component {
+class EditNoteForm extends Component {
 
     constructor(props) {
         super(props);
 
         this.state = {
-            title: '',
-            content: '',
-            tags: [],
+            id: props.note.id,
+            title: props.note.title,
+            content: props.note.content,
+            tags: props.note.tags,
             validationErrors: []
         };
 
@@ -23,7 +24,7 @@ class AddNoteForm extends Component {
 
     
     onTitleChange(event) {
-        const title = event.target.value.trim();
+        const title = event.target.value;
 
         this.validateTitle(title);
 
@@ -32,7 +33,7 @@ class AddNoteForm extends Component {
 
 
     onContentChange(event) {
-        const content = event.target.value.trim();
+        const content = event.target.value;
 
         this.validateContent(content);
         
@@ -41,7 +42,7 @@ class AddNoteForm extends Component {
 
 
     onTagsChange(event) {
-        const tags = event.target.value.trim();
+        const tags = event.target.value;
 
         if (this.validateTags(tags)) {            
             this.setState({ tags: tags.split(',')});
@@ -56,7 +57,12 @@ class AddNoteForm extends Component {
             const { title, content } = this.state;
             
             if (this.validateTitle(title) && this.validateContent(content)) {
-                this.props.onSaveNote(this.state);
+                this.props.onSaveNote({
+                    id: this.state.id,
+                    title: this.state.title,
+                    content: this.state.content,
+                    tags: this.state.tags
+                });
             }
         }
     }
@@ -145,33 +151,33 @@ class AddNoteForm extends Component {
         return (
             <div className="card card-body">
                 <div className="mb-2">        
-                    <span className="h4 my-auto"><i className="fa fa-file-text-o fa-lg"></i> New Note</span>
+                    <span className="h4 my-auto"><i className="fa fa-file-text-o fa-lg"></i> Edit Note</span>
                     <a className="float-right ml-auto" onClick={this.props.onCloseModal}>
-                        <i className="fa fa-remove fa-2x mr-2 text-danger"></i>
+                        <i className="fa fa-remove mr-2 fa-2x text-danger"></i>
                     </a>
                 </div>
                 {validationErrorSummary}
                 <form onSubmit={this.onSave} className="mt-2">
                     <div className="form-group">
                         <label htmlFor="title">Title</label>
-                        <input type="text" className="form-control" name="title" autoFocus onChange={this.onTitleChange} />
+                        <input type="text" className="form-control" name="title" autoFocus onChange={this.onTitleChange} value={this.state.title}/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="content">Content</label>
-                        <textarea className="form-control" name="content" rows="3" onChange={this.onContentChange}></textarea>
+                        <textarea className="form-control" name="content" rows="3" onChange={this.onContentChange} value={this.state.content}></textarea>
                     </div>
                     <div className="form-group">
                         <label htmlFor="tags">Tags</label>
-                        <input type="text" className="form-control" name="tags" onChange={this.onTagsChange} />
+                        <input type="text" className="form-control" name="tags" onChange={this.onTagsChange} value={this.state.tags.join(',')} />
                     </div>
                     <div className="form-group row">
                         <div className="col-sm-4 col-md-3 col-xl-2 ml-auto">
-                            <button type="submit" className="btn btn-success btn-lg btn-block">
+                            <button type="submit" className="btn btn-success btn-block">
                                 <i className="fa fa-save mr-2"></i>Save
                             </button>
                         </div>
                         <div className="col-sm-4 col-md-3 col-xl-2">
-                            <button className="btn btn-danger btn-lg btn-block mt-2 mt-sm-0"
+                            <button className="btn btn-danger btn-block mt-2 mt-sm-0"
                                 onClick={this.props.onCloseModal}
                                 type="button">
                                 <i className="fa fa-remove mr-2"></i>Cancel
@@ -184,9 +190,10 @@ class AddNoteForm extends Component {
     }
 }
 
-AddNoteForm.propTypes = {
+EditNoteForm.propTypes = {
+    note: PropTypes.object,
     onCloseModal: PropTypes.func,
     onSaveNote: PropTypes.func
 };
 
-export default AddNoteForm;
+export default EditNoteForm;
