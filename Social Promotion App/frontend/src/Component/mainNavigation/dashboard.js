62e0a68b870729c5da6d4e81bfbd2fae3eaafec5
adityachouhan <adityachouhan29@gmail.com>
2018-05-09 10:36:34 -0700

    removed debugger

--- Social Promotion App/frontend/src/Component/mainNavigation/dashboard.js
+++ Social Promotion App/frontend/src/Component/mainNavigation/dashboard.js
@@ -9,7 +9,7 @@ class Dashboard extends Component {
     this.state = {data: {
   columns: [
     ['Tweets', 0],
-    ['Blurbs', 0]
+    ['Images', 0]
   ],
   type : 'bar'
 }};
@@ -36,7 +36,6 @@ fetch('http://localhost:5000/dash', {
     }).then(r => r.json())
 	  .then(r => {
         console.log(r);
-		debugger;
 		var temp =r;
 		this.setState({data: temp});
 		console.log(this.state.data);
